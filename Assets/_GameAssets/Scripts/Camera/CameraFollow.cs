19c083d969a97c32858c132cc95b84db85a72401
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-11-11 14:11:52 +0100

    Sesion 2020/11/11

---
+++ Assets/_GameAssets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float xOffset;
+    void Update()
+    {
+        transform.position = new Vector3(
+            target.transform.position.x + xOffset, 
+            transform.position.y, 
+            transform.position.z);
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Varios/TwoPointsMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPointsMover : MonoBehaviour
+{
+    public Transform origen;
+    public Transform destino;
+    public GameObject objetoAMover;
+    public float speed;
+    private Vector2 nuevaPosicion;
+    private float pct=0f;//Porcentaje de desplazamiento
+    void Update()
+    {
+        nuevaPosicion = Vector2.Lerp(origen.position, destino.position, pct);
+        objetoAMover.transform.position = nuevaPosicion;
+        pct += Time.deltaTime * speed;
+        if (pct>=1)
+        {
+            pct = 1;
+            speed*= -1;
+            objetoAMover.transform.localScale = new Vector2(1, 1);
+        }
+        if (pct <= 0)
+        {
+            pct = 0;
+            speed*= -1;
+            objetoAMover.transform.localScale = new Vector2(-1, 1);
+        }
+    }
+}
